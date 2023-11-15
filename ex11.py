8bcde81f9fb834c23507b086157f016071182ff5
Othman Bella <147095415+Othman-Id-bella@users.noreply.github.com>
2023-11-16 00:19:45 +0100

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,7 @@
+A=int(input("enter the first nember:"))
+B=int(input("enter the second nember:"))
+
+if A*B>0:
+    print("The nembers are same sign ")
+else:
+    print("The numbers are not the same sign")
